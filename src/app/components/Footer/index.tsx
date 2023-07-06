fdc0154700bbc2a091be7e01c334d3adae8eedc6
Kuzmenko Oleksii <alexey.13.kuzmenko@gmail.com>
2023-07-06 23:47:18 +0300

    Review fixes

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -9,62 +9,48 @@ import YoutubeSVG from '@/app/assets/icons/youtube.svg';
 
 import styles from './index.module.scss';
 
-const SocialIcons = ({ iconSize }: { iconSize: number }) => {
-  return (
-    <div>
-      <Link
-        href="https://www.instagram.com/alekfrinth/"
-        className={styles.social}
-      >
-        <InstagramSVG width={`${iconSize}px`} height={`${iconSize}px`} />
-      </Link>
-      <Link href="https://twitter.com/AlekFrinth" className={styles.social}>
-        <TwitterSVG width={`${iconSize}px`} height={`${iconSize}px`} />
-      </Link>
-      <Link href="https://www.tiktok.com/@alekfrinth" className={styles.social}>
-        <TikTokSVG width={`${iconSize}px`} height={`${iconSize}px`} />
-      </Link>
-      <Link
-        href="https://www.youtube.com/channel/UCf_eyFlSKYJD1-uJQrNqUKg/featured"
-        className={styles.social}
-      >
-        <YoutubeSVG width={`${iconSize}px`} height={`${iconSize}px`} />
-      </Link>
-    </div>
-  );
-};
-
-const Mail = () => {
-  return (
-    <div className={styles.mail}>
-      <span>alekfrinth@gmail.com</span>
-    </div>
-  );
-};
-
-const Description = () => {
-  return (
-    <div className={styles.description}>
-      <span>© alekfrinth 2023</span>
-    </div>
-  );
-};
-
-const Contacts = () => {
-  return (
-    <div className={styles.contacts}>
-      <SocialIcons iconSize={42} />
-      <Mail />
-    </div>
-  );
-};
-
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        <Contacts />
-        <Description />
+        <div className={styles.contacts}>
+          <div className={styles['social-container']}>
+            <Link
+              href="https://www.instagram.com/alekfrinth/"
+              className={styles.social}
+              target="_blank"
+            >
+              <InstagramSVG width="42px" height="42px" />
+            </Link>
+            <Link
+              href="https://twitter.com/AlekFrinth"
+              className={styles.social}
+              target="_blank"
+            >
+              <TwitterSVG width="42px" height="42px" />
+            </Link>
+            <Link
+              href="https://www.tiktok.com/@alekfrinth"
+              className={styles.social}
+              target="_blank"
+            >
+              <TikTokSVG width="42px" height="42px" />
+            </Link>
+            <Link
+              href="https://www.youtube.com/channel/UCf_eyFlSKYJD1-uJQrNqUKg/featured"
+              className={styles.social}
+              target="_blank"
+            >
+              <YoutubeSVG width="42px" height="42px" />
+            </Link>
+          </div>
+          <div className={styles.mail}>
+            <span>alekfrinth@gmail.com</span>
+          </div>
+        </div>
+        <div className={styles.description}>
+          <span>© alekfrinth 2023</span>
+        </div>
       </div>
     </footer>
   );
