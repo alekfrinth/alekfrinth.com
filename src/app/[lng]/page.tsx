adf1e6d9a2a561845d2335b5046792de2b9d605b
Polina Baksheieva <55092422+Polina04@users.noreply.github.com>
2023-07-07 12:12:03 +0400

    add main page layout

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,6 +1,10 @@
 import { useTranslation } from '@/app/i18n';
+import Link from 'next/link';
 
+import Button from '@/app/components/Button';
 import Header from '@/app/components/Header';
+
+import styles from './index.module.scss';
 
 const Home = async ({ params: { lng } }: { params: { lng: string } }) => {
   const { t } = await useTranslation(lng, 'common');
@@ -8,6 +12,17 @@ const Home = async ({ params: { lng } }: { params: { lng: string } }) => {
   return (
     <>
       <Header lng={lng} />
+      <main className={styles.main}>
+        <Link href={`/${lng}/freedom-body`} className={styles.button}>
+          <Button>{t('body-freedom')}</Button>
+        </Link>
+        <Button color="secondary" disabled className={styles.button}>
+          {t('in-progress')}
+        </Button>
+        <Button color="tertiary" disabled className={styles.button}>
+          {t('in-progress')}
+        </Button>
+      </main>
     </>
   );
 };
