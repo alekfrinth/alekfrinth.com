f07773eda1f93e722b4f5c61f917f496dee02e77
Polina Baksheieva <55092422+Polina04@users.noreply.github.com>
2023-07-08 20:29:44 +0400

    add freedom body page layout

--- src/app/[lng]/freedom-body/page.tsx
+++ src/app/[lng]/freedom-body/page.tsx
@@ -1,10 +1,43 @@
-import Footer from '@/app/components/Footer';
-import Header from '@/app/components/Header';
+import { useTranslation } from '@/app/i18n';
 
-const FreedomBody = ({ params: { lng } }: { params: { lng: string } }) => {
+import BodySection from '@/app/components/BodySection';
+import MoreThanYearSection from '@/app/components/MoreThanYearSection';
+import PaintSection from '@/app/components/PaintSection';
+import PaintingsPart1 from '@/app/components/PaintingsPart1';
+import PaintingsPart2 from '@/app/components/PaintingsPart2';
+import PaintingsPart3 from '@/app/components/PaintingsPart3';
+import YoutubeVideo from '@/app/components/YoutubeVideo';
+import Footer from '@/app/components/common/Footer';
+import Header from '@/app/components/common/Header';
+
+import styles from './index.module.scss';
+
+const FreedomBody = async ({
+  params: { lng },
+}: {
+  params: { lng: string };
+}) => {
+  const { t } = await useTranslation(lng, 'common');
+
   return (
     <>
       <Header lng={lng} />
+
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <h1>{t('body-freedom')}</h1>
+          <p>{t('project-freedom-body')}</p>
+          <YoutubeVideo className={styles.video} />
+        </div>
+        <PaintingsPart1 lng={lng} />
+        <BodySection lng={lng} />
+        <PaintSection lng={lng} />
+        <PaintingsPart2 lng={lng} />
+        <MoreThanYearSection lng={lng} />
+        <PaintingsPart3 lng={lng} />
+        <p className={styles.report}>{t('wait-for-report')}</p>
+      </main>
+
       <Footer />
     </>
   );
