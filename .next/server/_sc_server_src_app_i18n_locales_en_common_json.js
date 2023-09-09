bdc8c16821a49fd681df24d074357bdff2c631ad
Polina Baksheieva <55092422+Polina04@users.noreply.github.com>
2023-09-09 16:32:02 +0300

    fix home page mobele layout

---
+++ .next/server/_sc_server_src_app_i18n_locales_en_common_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_sc_server_src_app_i18n_locales_en_common_json";
+exports.ids = ["_sc_server_src_app_i18n_locales_en_common_json"];
+exports.modules = {
+
+/***/ "(sc_server)/./src/app/i18n/locales/en/common.json":
+/*!*********************************************!*\
+  !*** ./src/app/i18n/locales/en/common.json ***!
+  \*********************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"body-freedom":"The Body of Freedom","in-progress":"in progress...","project-the-body-of-freedom":"The Body of Freedom project is a charitable endeavour, which aims to draw attention to the topic of war, raise funds for the Armed Forces of Ukraine, and to contribute to the culture with a history’s imprint of today’s Ukraine.","8-paintings":"With models’ bodies, eight canvases were created to be put up for an international auction. All proceeds will be donated to the Armed Forces of Ukraine.","painting-1":"solo artist\'s painting “he”","painting-2":"solo model\'s painting “she”","painting-3":"double painting “they”","cm":"{{size}} sm","acrylic":"acrylic","body-is":"The Body symbolizes mortality and physical sacrifice paid by Ukrainians, helping each other or protecting others on the battlefield. By exhibiting vulnerability and tenderness of our bodies, we remind of how frail life is. Keeping an austere look, applying paint layer by layer, and furthering our cause, we still head on – towards our goal.","paint-is":"The colours directly signify the Ukrainian flag that will remain on the canvases, through time and events, reminding the observer about the purpose of this project and the value of all memories of the war on the Ukrainian people.","canvas-no":"painting №{{number}}","more-than-year":"Over a year: every day and every moment. Transforming an idea into a plan, an opportunity – into motion, an action – into a weapon. Overcoming doubt and fear, striving for victory. Free and brave, strong and inspired. Invincible – we do everything to reclaim what is ours.","body-freedom-is":"“The Body of Freedom” is the body of each and every one who aids Ukraine in the fight for our home. Together, we have proven to the world more than once that Ukraine is all about strength, unity, and invincibility.","wait-for-report":"очікуйте звітність після продажу всіх полотен...","was-worked-on-by":"Implemented by","alek-frinth-name":"Alek Frinth","alek-frinth-job":"artist","anastasia-karooka-name":"Anastasiia Kuchmenko","anastasia-karooka-job":"model, executive director at “Teronlyfans”","katerina-anonimna-name":"Катерина Анонімна","katerina-anonimna-job":"модель","oleksandr-hrebeniuk-name":"Oleksandr Hrebeniuk","oleksandr-hrebeniuk-job":"photography","artem-palamarchuk-name":"Artem Palamarchuk","artem-palamarchuk-job":"videography, montage","polina-baksheieva-name":"Polina Baksheieva","polina-baksheieva-position":"frontend developer","vladyslav-marchenko-name":"Vladyslav Marchenko","vladyslav-marchenko-position":"devops engineer"}');
+
+/***/ })
+
+};
+;
